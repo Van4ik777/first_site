52ae1236ffd857c3e9b25beced023c432b64d58f
Van4ik777 <140610957+Van4ik777@users.noreply.github.com>
2024-03-07 22:22:56 +0200

    Update mainjs.js

--- templates/mainjs.js
+++ templates/mainjs.js
@@ -1,13 +1,1 @@
-<script>
-    // Функция для прокрутки страницы до второго вірша при загрузке страницы
-    window.onload = function() {
-        // Проверяем, если id равен 2, то прокручиваем страницу до второго вірша
-        var verseId = {{ verse.id }};
-        if (verseId === 2) {
-            var element = document.getElementById('verse-2');
-            if (element) {
-                element.scrollIntoView();
-            }
-        }
-    };
-</script>
+
